a9274e9fcbb8122ee4709458965c90250ecca954
Matthew Fornero <matt.fornero@mathworks.com>
2014-01-07 12:01:51 -0500

    mathworks/zynq: updated build.sh to allow local overrides
    Added ability for build.sh to incorperate .config param overrides
    defined in a .localconfig file in the buildroot root directory
    
    The .localconfig file will be appended to the generated defconfig
    prior to running make <>_defconfig
    
    Also added a --target|-t param to build.sh to allow a target
    other than all to be specified. Useful for downloading all sources
    prior to building a number of board targets

--- build.sh
+++ build.sh
@@ -8,36 +8,48 @@ BR_DIR=$( cd "$( dirname "$0" )" && pwd )
 DYNCONFIG=dynconfig
 OUTPUTDIR=${BR_DIR}/output
 MYSHELL=`echo $SHELL | sed -e 's/.*\///'`
-
+LOCALCONFIG=${BR_DIR}/.localconfig
 echoerr() { echo "Error: $@" 1>&2; }
 
 usage() {
 	cat >&2 <<EOL
 
-Usage: build.sh <board_dir> <configname>
+Usage: build.sh [options]<board_dir> <configname>
 Generate and build a buildroot dynamic configuration
 parameters:
 	<board_dir> 			path to the board directory containing a build.sh script
 	<configname>			arguement to pass to the build.sh script
+options:
+	--target|-t target		target to pass to make command, useful for downloading
+					sources before launching multiple builds
 EOL
 	exit 1
 }
 
 parse_cmds() {
 	CMD=$1
-	
+	ARGC=$#
 	case "${CMD}" in
 	--help|-h)
 		usage
+		;;
+	--target|-t)
+		shift		
+		MKTGT=$1
+		shift
+		((ARGC=ARGC-2))
 		;;
 	*)
 		;;
 	esac
 	
-	if [ "$#" -ne 2 ]; then
+	if [ "$ARGC" -ne 2 ]; then
 		echoerr "Incorrect syntext"	
 		usage
 	fi
+
+	BOARD_DIR=$1
+	CONFIG=$2
 }
 
 set_title() {
@@ -50,10 +62,13 @@ set_title() {
 
 }
 
-parse_cmds $@
+local_override() {
+	if [ -f ${LOCALCONFIG} ]; then
+		cat ${LOCALCONFIG} >> configs/${DYNCONFIG}_defconfig
+	fi
+}
 
-BOARD_DIR=$1
-CONFIG=$2
+parse_cmds $@
 BUILD_SH=${BOARD_DIR}/build.sh
 
 if [ ! -d ${BOARD_DIR} ]; then
@@ -69,6 +84,8 @@ fi
 # Execute the script
 ${BUILD_SH} ${BR_DIR} ${DYNCONFIG} ${CONFIG}
 if [ $? -eq 0 ]; then
+	# Apply any local overrides
+	local_override
 	# Navigate to the target directory
 	TGTDIR=${OUTPUTDIR}/${CONFIG}
 	mkdir -p ${TGTDIR}
@@ -76,7 +93,7 @@ if [ $? -eq 0 ]; then
 	make O=${TGTDIR} -C ${BR_DIR} ${DYNCONFIG}_defconfig
 	rm ${BR_DIR}/configs/${DYNCONFIG}_defconfig
 	set_title ${CONFIG}
-	make clean && make
+	make clean && make ${MKTGT}
 	set_title ${MYSHELL}
 fi
 
