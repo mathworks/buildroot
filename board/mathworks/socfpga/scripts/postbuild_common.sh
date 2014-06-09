24f17d352421f56de3c4845331369e85da298b63
Matthew Fornero <mfornero@mathworks.com>
2014-06-09 14:21:15 -0400

    mathworks/socfpga: initial commit of board files

---
+++ board/mathworks/socfpga/scripts/postbuild_common.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# format: <post-build-script.sh> <target dir> -b <board name> [-c <chip name>] [-a <application> [-a <application>]]
+# executed out of main buildroot source directory
+# available environment variables
+#	BUILDROOT_CONFIG: path to .config file
+#	HOST_DIR, STAGING_DIR, TARGET_DIR
+#	BINARIES_DIR: images dir
+#	BASE_DIR: base output directory
+
+SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
+source ${SCRIPT_DIR}/parse_opts.sh
+
+TARGET_DIR=${INDIR}
+
+###################################
+# Update inittab to not use getty
+###################################
+tempfile=$(mktemp)
+sed 's|\(.*\)::respawn.*# GENERIC_SERIAL$|\1::respawn:-/bin/sh # GENERIC_SERIAL|' ${TARGET_DIR}/etc/inittab > $tempfile && \
+	cat $tempfile > ${TARGET_DIR}/etc/inittab
+	
+#####################################
+# Add the version info to the rootfs	
+#####################################
+${SCRIPT_DIR}/git_verinfo.sh $BUILDROOT_CONFIG ${OUTPUT_DIR}/build $BR_ROOT ${TARGET_DIR}/etc/buildinfo
---
+++ board/mathworks/socfpga/sdcard/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Starting MathWorks Linux image..."
+
+cd /mnt
+
+# load the MW axi kernel module
+modprobe mwipcore
+#modprobe mwgeneric
+
+# run dhcp script to obtain IP Address
+#udhcpc -s /mnt/dhcp.script
+ifconfig eth0 192.168.1.101 netmask 255.255.255.0
