721106c31fb86b49a9655a14fbc31c7b4c7fe16b
Matthew Fornero <mfornero@mathworks.com>
2015-01-12 20:05:54 -0500

    uboot-tools: enable board-specific target tools
    Enable fw_printenv / fw_setenv to be built using the default
    environment for the system's uboot. This enables the target tools
    to re-create a default environment if the current environment is
    corrupt or does not exist

--- package/uboot-tools/uboot-tools.mk
+++ package/uboot-tools/uboot-tools.mk
@@ -4,18 +4,68 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_UBOOT_TOOLS_USECUSTOMGIT),y)
+UBOOT_TOOLS_VERSION     = $(call qstrip,$(BR2_TARGET_UBOOT_VERSION))
+UBOOT_TOOLS_BOARD_NAME = $(call qstrip,$(BR2_TARGET_UBOOT_BOARDNAME))
+UBOOT_TOOLS_SITE        = $(call qstrip,$(BR2_TARGET_UBOOT_CUSTOM_GIT_REPO_URL))
+UBOOT_TOOLS_SITE_METHOD = git
+UBOOT_TOOLS_CONFIGURE_OPTS = $(TARGET_CONFIGURE_OPTS)
+else
 UBOOT_TOOLS_VERSION = 2013.07
 UBOOT_TOOLS_SOURCE  = u-boot-$(UBOOT_TOOLS_VERSION).tar.bz2
 UBOOT_TOOLS_SITE    = ftp://ftp.denx.de/pub/u-boot
+endif
+
+UBOOT_TOOLS_MAKE_OPTS += \
+	HOSTCC="$(TARGET_CC)"		\
+	HOSTCFLAGS="$(TARGET_CFLAGS)"	\
+	HOSTLDFLAGS="$(TARGET_LDFLAGS)"	\
+	HOSTSTRIP=true
+
+
+ifeq ($(BR2_PACKAGE_UBOOT_TOOLS_USECUSTOMGIT),y)
+UBOOT_TOOLS_ARCH=$(KERNEL_ARCH)
+UBOOT_TOOLS_MAKE_OPTS += \
+	CROSS_COMPILE="$(CCACHE) $(TARGET_CROSS)" \
+	ARCH=$(UBOOT_TOOLS_ARCH)
+endif
+
 UBOOT_TOOLS_LICENSE = GPLv2+
 UBOOT_TOOLS_LICENSE_FILES = COPYING
 
+ifeq ($(BR2_PACKAGE_UBOOT_TOOLS_USEBOARDCONFIG),y)
+define UBOOT_TOOLS_CONFIGURE_CMDS
+    $(UBOOT_TOOLS_CONFIGURE_OPTS)   \
+        $(MAKE) -C $(@D) 			\
+        $(UBOOT_TOOLS_MAKE_OPTS)    \
+		$(UBOOT_TOOLS_BOARD_NAME)_config	
+	@echo >> $(@D)/include/config.h
+	@echo "/* Add a wrapper around the values Buildroot sets. */" >> $(@D)/include/config.h
+	@echo "#ifndef __BR2_ADDED_CONFIG_H" >> $(@D)/include/config.h
+	@echo "#define __BR2_ADDED_CONFIG_H" >> $(@D)/include/config.h
+	$(call insert_define,DATE,$(DATE))
+	$(call insert_define,CONFIG_LOAD_SCRIPTS,1)
+	$(call insert_define,CONFIG_IPADDR,$(BR2_TARGET_UBOOT_IPADDR))
+	$(call insert_define,CONFIG_GATEWAYIP,$(BR2_TARGET_UBOOT_GATEWAY))
+	$(call insert_define,CONFIG_NETMASK,$(BR2_TARGET_UBOOT_NETMASK))
+	$(call insert_define,CONFIG_SERVERIP,$(BR2_TARGET_UBOOT_SERVERIP))
+	$(call insert_define,CONFIG_ETHADDR,$(BR2_TARGET_UBOOT_ETHADDR))
+	$(call insert_define,CONFIG_ETH1ADDR,$(BR2_TARGET_UBOOT_ETH1ADDR))
+	@echo "#endif /* __BR2_ADDED_CONFIG_H */" >> $(@D)/include/config.h
+endef
+else
+define UBOOT_TOOLS_APPLY_CONFIG_PATCH
+    support/scripts/apply-patches.sh $(@D) package/uboot-tools uboot-tools-01-drop-configh-from-tools.patch.conditional
+endef    
+UBOOT_TOOLS_PRE_PATCH_HOOKS += \
+	UBOOT_TOOLS_APPLY_CONFIG_PATCH
+endif
+
+
 define UBOOT_TOOLS_BUILD_CMDS
-	$(MAKE) -C $(@D) 			\
-		HOSTCC="$(TARGET_CC)"		\
-		HOSTCFLAGS="$(TARGET_CFLAGS)"	\
-		HOSTLDFLAGS="$(TARGET_LDFLAGS)"	\
-		HOSTSTRIP=true			\
+	$(UBOOT_TOOLS_CONFIGURE_OPTS)   \
+	    $(MAKE) -C $(@D) 		    \
+		$(UBOOT_TOOLS_MAKE_OPTS)    \
 		tools env
 endef
 
