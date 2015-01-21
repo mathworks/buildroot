6653ea187c455628a3efd9d19c49893c3da38d0d
Abhijeet Gadkari <agadkari@agadkari-deb7-64>
2015-01-21 17:17:02 -0500

    Add zybo files and some init.sh update

--- board/mathworks/zynq/sdcard/init.sh
+++ board/mathworks/zynq/sdcard/init.sh
@@ -1,13 +1,46 @@
 #!/bin/sh
 
-echo "Starting MathWorks Linux image..."
+# Uncomment following lines for viewing verbose kernel debug messages
+# dmesg -n 8
+# echo 7 > /proc/sys/kernel/printk
 
+echo "+++ Configuring device hostname"
+if [ -f /mnt/hostname ]
+then
+    cp -f /mnt/hostname /etc/hostname
+else
+    echo "### hostname file not found in the SD card. Using default settings in /etc/hostname..."
+fi
+
+echo "+++ Configuring network interfaces"
+if [ -f /mnt/interfaces ]
+then
+    # Make the network-interface directories if not already present
+    cp -f /mnt/interfaces /etc/network/interfaces
+else
+    echo "### interfaces file not found in the SD card. Using default settings in /etc/interfaces..."
+fi
+
+# Restart network so that interfaces file change takes effect
+/etc/init.d/*network restart
+
+echo "### Starting MathWorks Linux image..."
 cd /mnt
 
+# Program the FPGA if system.bit.bin file is present
+if [ -f system.bit.bin ]
+then
+    /usr/bin/zynqfpgaprog system.bit.bin 
+fi
+
 # load the MW axi kernel module
-modprobe mwipcore
+modprobe mwadma 
 modprobe mwgeneric
 
-# run dhcp script to obtain IP Address
-#udhcpc -s /mnt/dhcp.script
-ifconfig eth0 192.168.1.101 netmask 255.255.255.0
+# restore boot-environment: required for automated test env.
+# test will overwrite this to use axi4stream
+cp -f _default_uboot_env_ uboot.env
+rm -f system.bit.bin
+sync
+sync
+
