460a9343f9a25e4eae0fef9dff2ceb442e9e6de3
Matthew Fornero <mfornero@mathworks.com>
2015-10-13 16:05:35 -0400

    mathworks/common: add function to get src dir
    Add a helper function to find the source dir, when it may be
    overriden by a local.mk file

--- board/mathworks/common/scripts/helper_func.sh
+++ board/mathworks/common/scripts/helper_func.sh
@@ -20,6 +20,39 @@ function print_err() {
 
 function get_cfg_var() {
 	local var=$1
-	res=`cat $BR2_CONFIG | grep ^$var | sed -e 's/BR2_.*=//' | tr -d '"'`
+	res=$(cat $BR2_CONFIG | grep "^${var} *=" | sed -e 's/BR2_.*=//' | tr -d '"')
+}
+
+function get_src_dir() {
+    local pkg=$1
+    local override_info
+    local build_dir
+    # Check if we're overriding the source
+    if [ -f ${BASE_DIR}/local.mk ]; then
+        override_file=${BASE_DIR}/local.mk
+    fi
+    build_dir=$( cd "$( dirname "$BR2_CONFIG" )" && pwd )
+    if [ -f ${build_dir}/local.mk ]; then
+        override_file=${build_dir}/local.mk
+    fi
+
+    if [ ! -z "$override_file" ]; then
+        # grab the source file from the override dir
+        res=$(cat $override_file | grep -i ${pkg}_override_srcdir | sed -e 's/.*= *//')
+    else
+        # Otherwise check the version and point to the build dir
+        case $pkg in
+            linux)
+                get_cfg_var 'BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION'
+                res=${BUILD_DIR}/linux-${res}
+                ;;
+            uboot)
+                get_cfg_var 'BR2_TARGET_UBOOT_CUSTOM_REPO_VERSION'
+                res=${BUILD_DIR}/uboot-${res}
+                ;;
+            *)
+                ;;
+        esac
+    fi
 }
 
