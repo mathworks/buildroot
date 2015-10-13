6bba7ef65691e5b416e65c011b044a245220fbcb
Matthew Fornero <mfornero@mathworks.com>
2015-10-13 16:07:47 -0400

    mathworks/common: Update gen_dtb to use CPP
    Follow the latest kernel and run the .dts/.dtsi files through
    CPP before calling DTC. This allows the use of #include, which
    some of the newer kernel sources use

--- board/mathworks/common/scripts/gen_dtb.sh
+++ board/mathworks/common/scripts/gen_dtb.sh
@@ -20,8 +20,8 @@ source ${SCRIPT_DIR}/helper_func.sh
 
 # Get the linux version from the BR .config file
 LINUX_INFO='BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION'
-get_cfg_var $LINUX_INFO
-LINUX_VER=$res
+get_src_dir linux
+LINUX_DIR=$res
 
 # Get the base devicetree name
 DTS_INFO='BR2_LINUX_KERNEL_INTREE_DTS_NAME'
@@ -30,8 +30,29 @@ DTS_BASE=${res}.dts
 
 # Tool paths
 DTC=${HOST_DIR}/usr/bin/dtc
-LINUX_DTS=${BUILD_DIR}/linux-${LINUX_VER}/arch/arm/boot/dts
+LINUX_DTS=${LINUX_DIR}/arch/arm/boot/dts
 DTS_FILE=devicetree.dts
+
+
+# Determine the toolchain name for CPP
+get_cfg_var 'BR2_ARM_EABI'
+EABI=$res
+get_cfg_var 'BR2_ARM_EABIHF'
+EABIHF=$res
+if [ "$EABI" == "y" ]; then
+    TC_PREFIX="arm-linux-gnueabi"
+elif [ "$EABIHF" == "y" ]; then
+    TC_PREFIX="arm-linux-gnueabihf"
+else
+    print_err "Could not find arachitecture"
+fi
+
+DTC_CPP_FLAGS="-Wp,-MD,dependency.pre.tmp -nostdinc \
+            -I${COMMON_DIR}/dts \
+            -I${BOARD_DIR}/dts \
+            -I${LINUX_DTS} \
+            -I${LINUX_DTS}/include \
+            -undef -D__DTS__  -x assembler-with-cpp"
 
 pushd ${SD_DIR}
 for APP_NAME in ${APP_LIST}; do
@@ -39,12 +60,18 @@ for APP_NAME in ${APP_LIST}; do
     if [ -f ${BOARD_DIR}/dts/${APP_DTSI} ]; then
         print_msg "Generating ${APP_NAME} dtb"
         DTB_FILE=devicetree_${APP_NAME}.dtb
-# Generate a temporary DTS file
+        # Generate a temporary DTS file
 cat << EOF > ${DTS_FILE}
-    /include/ "${DTS_BASE}"
-    /include/ "${APP_DTSI}"
+    #include "${DTS_BASE}"
+    #include "${APP_DTSI}"
 EOF
-        ${DTC} -i ${LINUX_DTS} -i ${BOARD_DIR}/dts -i ${COMMON_DIR}/dts -I dts -O dtb -o ${DTB_FILE} ${DTS_FILE}
+        # Run the DTS through CPP     
+        ${HOST_DIR}/usr/bin/${TC_PREFIX}-cpp $DTC_CPP_FLAGS -o ${APP_NAME}.tmp.dts ${DTS_FILE}
+        # Call DTC
+        ${DTC} -i ${LINUX_DTS} -i ${BOARD_DIR}/dts -i ${COMMON_DIR}/dts -I dts -O dtb -o ${DTB_FILE} ${APP_NAME}.tmp.dts
+        # Cleanup
+        rm -f dependency.pre.tmp
+        rm -f ${APP_NAME}.tmp.dts
         rm -f ${DTS_FILE}
     fi
 done
