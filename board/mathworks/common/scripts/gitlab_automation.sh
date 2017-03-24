dbf8dd8588d6eb477e381d20ac84c39872d580fd
Matt Fornero <matt.fornero@mathworks.com>
2017-03-24 16:13:37 -0400

    gitlab-ci: Use CONFIG_ prefix for script vars
    Use the CONFIG_ prefix instead of the CI_ prefix to disambiguate between
    Gitlab internal variables and those defined for our scripts

--- board/mathworks/common/scripts/gitlab_automation.sh
+++ board/mathworks/common/scripts/gitlab_automation.sh
@@ -5,8 +5,8 @@
 #############################
 platform_variable() {
 	local suffix=$1
-	local platName=${CI_JOB_PLATFORM^^}
-	local varName=${platName}_${suffix}
+	local platName=${CONFIG_JOB_PLATFORM^^}
+	local varName=CONFIG_${platName}_${suffix}
 	local varValue=${!varName}
 	echo $varValue
 }
@@ -48,7 +48,7 @@ run_build_command() {
 	local extraargs=$@
 	local brvars=$(resolve_br_vars)
 	set -x
-	${CI_PROJECT_DIR}/build.py --target "$target" --dl $CI_BR2_DL_DIR/$CI_JOB_PLATFORM -b $CI_JOB_BOARD -p $CI_JOB_PLATFORM --ccache -l logs/${CI_BUILD_NAME}.log $brvars $extraargs
+	${CI_PROJECT_DIR}/build.py --target "$target" --dl $CONFIG_BR2_DL_DIR/$CONFIG_JOB_PLATFORM -b $CONFIG_JOB_BOARD -p $CONFIG_JOB_PLATFORM --ccache -l logs/${CI_BUILD_NAME}.log $brvars $extraargs
 	set +x
 }
 
@@ -92,21 +92,21 @@ case "${CI_BUILD_STAGE}" in
 		;;
 	sources_common)
 	  	echo "Preparing Common Sources"
-		CI_JOB_PLATFORM=zynq
-		CI_JOB_BOARD=zed
+		CONFIG_JOB_PLATFORM=zynq
+		CONFIG_JOB_BOARD=zed
 		run_build_command source --ccache-clean
-		rm -rf $CI_BR2_DL_DIR/zynq/linux-*
-		rm -rf $CI_BR2_DL_DIR/zynq/uboot-*
-		cp -R $CI_BR2_DL_DIR/zynq $CI_BR2_DL_DIR/socfpga
-		cp -R $CI_BR2_DL_DIR/zynq $CI_BR2_DL_DIR/zynqmp
+		rm -rf $CONFIG_BR2_DL_DIR/zynq/linux-*
+		rm -rf $CONFIG_BR2_DL_DIR/zynq/uboot-*
+		cp -R $CONFIG_BR2_DL_DIR/zynq $CONFIG_BR2_DL_DIR/socfpga
+		cp -R $CONFIG_BR2_DL_DIR/zynq $CONFIG_BR2_DL_DIR/zynqmp
 		rm -rf ${CI_PROJECT_DIR}/output/*/build
 		;;
 	sources_custom)
-		echo "Preparing $CI_JOB_PLATFORM Sources"	
+		echo "Preparing $CONFIG_JOB_PLATFORM Sources"	
 		run_build_command source -u
 		;;
 	build)
-		echo "Building $CI_JOB_BOARD/$CI_JOB_PLATFORM"
+		echo "Building $CONFIG_JOB_BOARD/$CONFIG_JOB_PLATFORM"
 		run_build_command "all legal-info" -u -q
 		;;
 	*)
