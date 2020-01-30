6957b10d15481a9afb05315f8534b999a6f5aed9
root <root@hyd-kakkala-deb9-64>
2020-01-30 20:28:46 +0530

    Updating the rftool and making rfdc config specific to Zcu111

---
+++ board/mathworks/zynqmp/boards/zcu111/sdcard/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add any custom code to be run at startup here
+is_rfdc_exist=$(ls /sys/bus/platform/devices/*)
+if [[ $is_rfdc_exist = *usp_rf_data_converter* ]]; then
+   echo "Running rftool application..."
+   rftool
+   echo "rftool application closed"
+fi
